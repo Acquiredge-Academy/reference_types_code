16203eb02d0ff2ee6eb832f04f38d67fa06a896d
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-03 02:14:26 +0000

    Completed Function Expressions and Hoisting

--- functions/functionExpressionsAndHoisting.js
+++ functions/functionExpressionsAndHoisting.js
@@ -1,5 +1,15 @@
-
+walk(); // WORKS!
 // Function Declaration
 function walk() {
     console.log("walk");
 }
+run(); // Does not work
+// Function Expressions
+let run = function() {
+    console.log("run");
+};
+move(); // Does not work
+// Arrow Functions
+let move = () => {
+    console.log("move");
+};
