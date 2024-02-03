47c82a65c39fe83b4f5dc53232a911e4d3da8f1b
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-03 02:01:00 +0000

    Template Literals

--- template_literals/example.js
+++ template_literals/example.js
@@ -2,3 +2,7 @@ let student = {
     firstName: "Jeff",
     lastName: "Winters"
 };
+
+// let sentence = "The student's full name is " + student.firstName + " " + student.lastName + ".";
+let sentence = `The student's full name is ${student.firstName} ${student.lastName}.`;
+console.log(sentence);
