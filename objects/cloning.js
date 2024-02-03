c05d5f61c8a8daabc07e6a2cda02f2a0e09f74f1
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-03 00:54:51 +0000

    Object Cloning

--- objects/cloning.js
+++ objects/cloning.js
@@ -1,5 +1,19 @@
 let student = {
     name: "Kevin",
     age: 16,
-    year: "Sophomore"
+    year: "Sophomore",
+    grades: { math: 90 }
 }
+
+// const another = {};
+// for(let key in student) {
+//     another[key] = student[key];
+// }
+
+// const another = {...student};
+
+const another = Object.assign({}, student);
+student.grades.math = 95;
+
+console.log(student);
+console.log(another);
