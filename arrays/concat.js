25fe7dc8c9fdbad29322be3083c6ad593011e0aa
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-03 01:51:02 +0000

    Adding comments to concat()

--- arrays/concat.js
+++ arrays/concat.js
@@ -1,6 +1,11 @@
 const array1 = ['a', 'b', 'c'];
 const array2 = ['d', 'e', 'f'];
+// Array.concat(): combines the elements of 2 or more arrays and returns a new array with all the elements
 const array3 = array1.concat(array2);
 
 console.log(array3);
-// Expected output: Array [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+// Expected output: [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+
+// concat() is not destructive: it doesn't affec the original arrays
+console.log(array1);
+// Expected output: [ 'a', 'b', 'c' ]
