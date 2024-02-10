b97c92891009490ac679685be0f2cfa652aadf60
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-10 03:02:30 +0000

    Exercise 5 initial

---
+++ exercises/exercise5.js
@@ -0,0 +1,28 @@
+/**
+ * Exercise 5: Grocery List Checker
+ * You have a shopping list of items stored in the array `shoppingList`.
+ * Use the Array forEach() method to iterate through shoppingList and print
+ * each item with its purchase status.
+ *
+ * Example Output:
+ * Apples - purchased
+ * Bread - not purchased
+ * Carrots - purchased
+ * Dairy Milk - not purchased
+ * Eggs - purchased
+ *
+ * DO NOT use a for loop for this. Use the forEach() method.
+ * The forEach() method is a higher order function, and takes a callback function.
+ * Read about the forEach() method here:
+ * https://www.geeksforgeeks.org/javascript-array-foreach-method/#
+ */
+
+const shoppingList = [
+    { item: "Apples", purchased: true },
+    { item: "Bread", purchased: false },
+    { item: "Carrots", purchased: true },
+    { item: "Dairy Milk", purchased: false },
+    { item: "Eggs", purchased: true }
+];
+
+// Code goes here
