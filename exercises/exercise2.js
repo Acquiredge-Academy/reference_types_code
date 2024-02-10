a5fd9c93b9b6edec5ee9475311219ff0b815ebd4
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-10 00:19:47 +0000

    Exercise 2 initial

---
+++ exercises/exercise2.js
@@ -0,0 +1,32 @@
+// Exercise 2: Object Equality
+// Write a function that compares two shallow objects and checks if they are equal
+function isEqual(obj1, obj2) {
+
+}
+
+// Examples
+const person1 = {
+    name: "John",
+    age: 25,
+    favoriteFood: "Pizza"
+};
+const person2 = {
+    name: "John",
+    age: 25,
+    favoriteFood: "Pizza"
+};
+const person3 = {
+    age: 25,
+    favoriteFood: "Pizza",
+    name: "John"
+};
+const person4 = {
+    name: "John",
+    age: 25,
+    favoriteFood: "Pizza",
+    location: "Brooklyn"
+};
+
+console.log(isShallowEqual(person1, person2)); // Expected: true
+console.log(isShallowEqual(person1, person3)); // Expected: true
+console.log(isShallowEqual(person1, person4)); // Expected: false
