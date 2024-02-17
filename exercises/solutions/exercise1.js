39fe494a47176cbb957015e7ef1208e680d19b49
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-02-17 01:44:45 +0000

    Implement exercise 1

--- exercises/solutions/exercise1.js
+++ exercises/solutions/exercise1.js
@@ -27,16 +27,20 @@ const school = {
 };
 
 // Task 1: Print the name of the city where the school is located
-
+console.log(school.address.city);
 
 // Task 2: Change the head of the science department to: "Dr.Turing"
-
-
+school.departments.science.head = "Dr.Turing";
+console.log(school.departments.science.head);
 // Task 3: Add a new class named "Drama" to the arts department with 15 students and no lab requirement
-
-
+school.departments.arts.classes.push({ name: "Drama", students: 15, isLabRequired: false });
+console.log(school.departments.arts.classes);
 // Task 4: Increase the number of students in the "Physics" class by 5
-
-
+school.departments.science.classes[0].students += 5;
+console.log(school.departments.science.classes);
 // Task 5: Create an array containing the names of all classes in the science department
-
+let classNames = [];
+for(let i = 0; i < school.departments.science.classes.length; i++){
+    classNames.push(school.departments.science.classes[i].name);
+}
+console.log(classNames);
